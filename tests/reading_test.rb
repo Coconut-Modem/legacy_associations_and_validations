aed216bd272dd8ff9db26095f50688e199775ec9
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-25 15:37:11 -0400

    Add lessons and readings w/ has_many relationship

--- tests/reading_test.rb
+++ tests/reading_test.rb
@@ -21,8 +21,19 @@ ApplicationMigration.migrate(:up)
 # Finally!  Let's test the thing.
 class ApplicationTest < Minitest::Test
 
-  def test_truth
-    assert true
+  def test_can_create_a_new_reading
+    lesson = Reading.create(caption: "Chapter 1")
+    assert Reading
+  end
+
+  def test_reading_id_exists
+    reading = Reading.create(caption: "Chapter 1")
+    assert_equal true, reading.id?
+  end
+
+  def test_can_retrieve_lesson_caption
+    reading = Reading.create(caption: "Chapter 1")
+    assert_equal "Chapter 1", reading.caption
   end
 
 end
