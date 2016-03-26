0f62440c4fa35ceff7cfad1476929a0986f91e1b
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-26 15:30:31 -0400

    Add courses/lessons validation w/ dependent destroy

--- tests/course_test.rb
+++ tests/course_test.rb
@@ -21,8 +21,40 @@ ApplicationMigration.migrate(:up)
 # Finally!  Let's test the thing.
 class ApplicationTest < Minitest::Test
 
-  def test_truth
-    assert true
+  def test_can_create_a_new_course
+    course = Course.create(name: "Coding 101")
+    assert Course
+  end
+
+  def test_course_id_exists
+    course = Course.create(name: "Coding 101")
+    assert_equal true, course.id?
+  end
+
+  def test_can_retrieve_course_name
+    course = Course.create(name: "Coding 101")
+    assert_equal "Coding 101", course.name
+  end
+
+  def test_course_can_have_many_lessons
+    course = Course.create(name: "Coding 101")
+    lesson_1 = Lesson.create(name: "How to Ruby")
+    lesson_2 = Lesson.create(name: "How to JavaScript")
+
+    course.add_lesson(lesson_1)
+    course.add_lesson(lesson_2)
+
+    assert_equal [lesson_1, lesson_2], course.lessons
+  end
+
+  def test_lessons_are_destroyed_when_course_is_destroyed
+    course = Course.create(name: "Coding 101")
+    lesson_1 = Lesson.create(name: "How to Ruby")
+    lesson_2 = Lesson.create(name: "How to JavaScript")
+    course.add_lesson(lesson_1)
+    course.add_lesson(lesson_2)
+    course.destroy
+    assert_equal [], course.lessons
   end
 
 end
