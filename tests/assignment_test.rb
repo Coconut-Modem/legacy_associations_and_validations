e90f003e814ea518504e0ad1d29b619d2cf478ad
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-26 21:53:41 -0400

    Issue adding assignment to lesson

---
+++ tests/assignment_test.rb
@@ -0,0 +1,29 @@
+# Basic test requires
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# Include both the migration and the app itself
+require './migration'
+require './application'
+
+# Overwrite the development database connection with a test connection.
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'test.sqlite3'
+)
+
+# Gotta run migrations before we can run tests.  Down will fail the first time,
+# so we wrap it in a begin/rescue.
+begin ApplicationMigration.migrate(:down); rescue; end
+ApplicationMigration.migrate(:up)
+
+
+# Finally!  Let's test the thing.
+class ApplicationTest < Minitest::Test
+
+  def test_assignment_can_be_created
+    assignment = Assignment.create(name: "Write a Program")
+    assert Assignment
+  end
+
+end
